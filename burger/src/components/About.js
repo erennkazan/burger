b1466a80af672d880a6057312f73ac00de4d02c5
erennkazan@gmail.com <erennkazan@gmail.com>
2023-07-26 21:04:57 +0300

    contact components created

--- burger/src/components/About.js
+++ burger/src/components/About.js
@@ -1,8 +1,16 @@
 import React from 'react'
-
+import BannerYeni from '../assets/banneryeni.jpg'
+import '../styles/About.css'
 function About() {
   return (
-    <div>About</div>
+    <div className='about'>
+       <div className='aboutTop' style={{backgroundImage:`url(${BannerYeni})`}}>
+       </div>
+       <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste nostrum iure magni doloribus odit quas, vitae porro facere dolorem impedit temporibus, aspernatur ad cupiditate sed dignissimos enim sapiente, quae beatae! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reiciendis veniam dolorem labore facilis, itaque odio voluptas quis corporis et magni! Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio quo, excepturi animi nostrum quas voluptas magni eligendi iste, eius quia natus possimus iure quod error modi quidem quibusdam esse aspernatur facilis fugit. Ad laboriosam est rem explicabo libero impedit suscipit quisquam error sunt, tempore corrupti soluta sequi magni consequatur excepturi!</p>
+       </div>
+    </div>
   )
 }
 
--- burger/src/components/Contact.js
+++ burger/src/components/Contact.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Banner from '../assets/banner.png'
+import '../styles/Contact.css'
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact'>
+      <div className='leftSide'  style={{backgroundImage:`url(${Banner})`}}>
+        
+      </div>
+      <div className='rightSide'>
+       <h1>Bizimle İletişime Geçin</h1>
+       <form>
+        <label>Ad Soyad</label>
+        <input type="text" name='name' placeholder='Lütfen Adınızı Soyadınızı giriniz...'/>
+        <label>E-mail</label>
+        <input type="email" name='email' placeholder='Lütfen e-mailinizi giriniz...'/>
+        <label>Mesaj</label>
+        <textarea rows="6" name='mesage' placeholder='Lütfen mesajınızı giriniz...'>
+        </textarea>
+       </form>
+      </div>
+    </div>
   )
 }
 
